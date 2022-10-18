2c1866065688c49024c03cfdfa8e28ef94ab0073
jaehyeonha <hyunymini0221@gmail.com>
2022-10-18 14:57:55 +0900

    lab7 play & make

---
+++ lab7_s2021115737/sigdemo3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+int time;
+
+int main()
+{
+	void f(int);			
+    time = 0;
+
+	signal(SIGINT, f);	
+    printf("you can't stop me!\n");
+	while(1){
+        sleep(1);
+        time++;
+        printf("haha\n");
+    }
+
+    return 0;
+}
+
+void f(int signum)
+{
+    printf("Currently elapsed time: %d sec(s)\n",time);
+}
