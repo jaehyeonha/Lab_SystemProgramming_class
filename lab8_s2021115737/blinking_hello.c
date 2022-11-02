a9e2819584901cd50374260652fdf8a5c8a11ba2
jaehyeonha <hyunymini0221@gmail.com>
2022-11-02 10:40:59 +0900

    blinking_hello while

--- lab8_s2021115737/blinking_hello.c
+++ lab8_s2021115737/blinking_hello.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <curses.h>
+#include <unistd.h>
+#include <termios.h>
 
-int main()
-{
+
+int main() {
+    struct termios info, new_info;
+    char input;
+
     initscr();
     clear();
 
-    move(10,20);
-    addstr("Hello, world");
-    move(LINES-1,0);
-    refresh();
-    sleep(1);
-        
-    move(10,20);
-    addstr("            ");
-    move(LINES-1,0);
-    refresh();
-    sleep(1);
-    
-    move(10,20);
-    addstr("Hello, world");
-    move(LINES-1,0);
-    refresh();
+    tcgetattr(STDIN_FILENO, &info);
+    new_info = info;
+    new_info.c_lflag &= ~ICANON;
+    new_info.c_lflag &= ~ECHO;
+    new_info.c_cc[VMIN] = 0;
+    new_info.c_cc[VTIME] = 0;
 
-    getch();
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_info);
+
+    while(1) {
+        if(read(STDIN_FILENO, &input, 1) > 0)
+            break;
+
+        move(10, 20);
+        addstr("Hellow, world");
+        move(LINES-1, 0);
+        refresh();
+
+        sleep(1);
+
+        move(10, 20);
+        addstr("             ");
+        move(LINES-1, 0);
+        refresh();
+
+        sleep(1);
+    }
+    
+    tcsetattr(STDIN_FILENO, TCSANOW, &info);
     endwin();
 
     return 0;
